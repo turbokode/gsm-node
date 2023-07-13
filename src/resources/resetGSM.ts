43d39b8d2bb12267cec30d073ea13e3a0e4b1159
Dario Pacule <dariopacule@gmail.com>
2023-07-13 04:40:25 +0200

    Execute command refactoring

--- src/resources/resetGSM.ts
+++ src/resources/resetGSM.ts
@@ -5,7 +5,7 @@ import { ReadlineParser, SerialPort } from "serialport";
 // Configuração do pino GPIO 23 como saída em nível alto (HIGH)
 const resetPin = new Gpio(23, "high");
 
-export async function resetGSM(
+export function resetGSM(
   port: SerialPort<AutoDetectTypes>,
   parser: ReadlineParser,
   callBack?: () => Promise<void>
@@ -13,41 +13,45 @@ export async function resetGSM(
   let tryCounter = 0;
   console.log("Trying to restart GSM!");
 
-  try {
-    // Coloca o pino em nível baixo (LOW) por um curto período de tempo
-    resetPin.writeSync(0);
-    await sleep(100);
+  return new Promise(async (resolve, reject) => {
+    try {
+      // Coloca o pino em nível baixo (LOW) por um curto período de tempo
+      resetPin.writeSync(0);
+      await sleep(100);
 
-    // Volta o pino ao nível alto (HIGH)
-    resetPin.writeSync(1);
+      // Volta o pino ao nível alto (HIGH)
+      resetPin.writeSync(1);
 
-    const interval = setInterval(() => {
-      if (tryCounter > 5) {
-        clearInterval(interval);
-        parser.removeListener("data", onData);
-        throw new Error("Time to try connect to GSM expired!");
-      }
+      const interval = setInterval(() => {
+        if (tryCounter > 5) {
+          clearInterval(interval);
+          parser.removeListener("data", onData);
+          throw new Error("Time to try connect to GSM expired!");
+        }
 
-      port.write("AT\r\n");
-      tryCounter++;
-    }, 10000);
+        port.write("AT\r\n");
+        tryCounter++;
+      }, 10000);
 
-    const onData = async (data: string) => {
-      console.log("RESET DATA: ", [data]);
+      const onData = async (data: string) => {
+        console.log("RESET DATA: ", [data]);
 
-      if (data.replace("\r", "") === "OK") {
-        clearInterval(interval);
-        parser.removeListener("data", onData);
+        if (data.replace("\r", "") === "OK") {
+          clearInterval(interval);
+          parser.removeListener("data", onData);
 
-        callBack && (await callBack());
+          callBack && (await callBack());
 
-        console.log("GSM Module restarted!");
-      }
-    };
-    parser.on("data", onData);
-  } catch (error) {
-    console.error("Error when try to restart GSM module:", error);
-  }
+          console.log("GSM Module restarted!");
+          resolve;
+        }
+      };
+      parser.on("data", onData);
+    } catch (error) {
+      console.error("Error when try to restart GSM module:", error);
+      reject;
+    }
+  });
 }
 
 function sleep(ms: number) {
