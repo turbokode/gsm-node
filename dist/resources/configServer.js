d26a1cd468fe78699e61cd346e0994e8d352218f
Dario Pacule <dariopacule@gmail.com>
2023-08-28 12:57:02 +0200

    Parar de rastrear temp_output.txt

--- dist/resources/configServer.js
+++ dist/resources/configServer.js
@@ -17,13 +17,17 @@ function configServer() {
         tryCounter++;
         return new Promise((resolve, reject) => {
             server_1.api
-                .post("/configSystem/save", { smsServerURL: process.env.GSM_PORT })
+                .post("/configSystem/save", { smsServerURL: process.env.GSM_PORT }, {
+                headers: {
+                    Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE2OTMwMjYxMzYsImV4cCI6MTAzMzI5Mzk3MzYsInN1YiI6ImQ2MTY4ODk3LWI1MGMtNGE5Yy04ZTg3LWZmYjNlMmY1ZTBmNSJ9.1a9lMAmQjIEoVWUkAnNa46K-aJbSSmLbiFjfdjPlcoQ",
+                },
+            })
                 .then(({ data }) => {
                 resolve(JSON.stringify(data));
             })
                 .catch((err) => __awaiter(this, void 0, void 0, function* () {
-                if ((tryCounter = 5))
-                    reject("Failed when try to send SMS Server URL to API!");
+                if (tryCounter === 50)
+                    reject(`Failed when try to send SMS Server URL to API! \n==========\n${err} `);
                 yield configServer();
             }))
                 .finally(() => {
